df7fa56a375d1430a77a72625b50589c6e478f87
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-03 15:03:55 +0000

    Deploy vvkk77/approver-dashboard to github.com/vvkk77/approver-dashboard.git:gh-pages

---
+++ precache-manifest.17cee7241456bc39811918f2ac864ae9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11bca55b3a302bf0978",
+    "url": "/approver-dashboard/css/app.6df05ae8.css"
+  },
+  {
+    "revision": "ad115dd1a3a1b56dbcb9bb08c4a779d2",
+    "url": "/approver-dashboard/img/empty-table.ad115dd1.png"
+  },
+  {
+    "revision": "423d5a42de0d7207f7d70a052f86593c",
+    "url": "/approver-dashboard/img/login-bg.423d5a42.png"
+  },
+  {
+    "revision": "0d13956bded0bd3a21d6afc448b20d5b",
+    "url": "/approver-dashboard/index.html"
+  },
+  {
+    "revision": "b11bca55b3a302bf0978",
+    "url": "/approver-dashboard/js/app.c53641f6.js"
+  },
+  {
+    "revision": "45554bb535df681a0efd",
+    "url": "/approver-dashboard/js/chunk-vendors.6db40986.js"
+  },
+  {
+    "revision": "e879279f7c107d9faf44f0333b34acef",
+    "url": "/approver-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/approver-dashboard/robots.txt"
+  }
+]);
