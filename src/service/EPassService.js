78f12a279e7e02346cdc29b3ed9808435b9cb7b0
Bernard Adongo <bernard.adongo@gmail.com>
2020-04-08 13:12:08 +0300

    updated api url

--- src/service/EPassService.js
+++ src/service/EPassService.js
@@ -5,7 +5,8 @@ import dotprop from 'dot-prop';
 import { isProd, isGithub } from '../utils/helpers';
 
 const BASE_URL = (() => {
-    const DEFAULT_API = 'https://viruscorona.co.in';
+    //const DEFAULT_API = 'https://viruscorona.co.in';
+    const DEFAULT_API = 'https://localhost:8080';
     const EPASS_API = `${window.location.protocol}//${window.location.hostname}/ecurfew`;
 
     if (!isProd || isGithub) {
